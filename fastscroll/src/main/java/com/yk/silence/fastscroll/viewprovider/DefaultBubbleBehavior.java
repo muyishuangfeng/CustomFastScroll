e0e56dc6ad323e6f53e7e1983a4e847c92d0266f
silence <yangkemuyi@sina.com>
2019-09-02 10:31:08 +0800

    first commit

---
+++ fastscroll/src/main/java/com/yk/silence/fastscroll/viewprovider/DefaultBubbleBehavior.java
@@ -0,0 +1,36 @@
+package com.yk.silence.fastscroll.viewprovider;
+
+import com.yk.silence.fastscroll.impl.ViewBehavior;
+
+/**
+ * Created by Michal on 11/08/16.
+ */
+public class DefaultBubbleBehavior implements ViewBehavior {
+
+    private final VisibilityAnimationManager animationManager;
+
+    public DefaultBubbleBehavior(VisibilityAnimationManager animationManager) {
+        this.animationManager = animationManager;
+    }
+
+    @Override
+    public void onHandleGrabbed() {
+        animationManager.show();
+    }
+
+    @Override
+    public void onHandleReleased() {
+        animationManager.hide();
+    }
+
+    @Override
+    public void onScrollStarted() {
+
+    }
+
+    @Override
+    public void onScrollFinished() {
+
+    }
+
+}
